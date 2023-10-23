4fa419f3e81558e77e5a32ff55dd4a0203729c45
shohcodes <zokirislom01@gmail.com>
2023-10-23 17:33:16 +0500

    model fields updated

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 from apps.core.models import TimeStampedModel
 from apps.users.choices import UsersRoleChoices
+from apps.users.validators import phone_validator
 
 
 class Users(TimeStampedModel):
     fullname = models.CharField(max_length=255)
-    phone_number = models.IntegerField()
+    phone_number = models.IntegerField(validators=[phone_validator])
     password = models.CharField(max_length=255)
     role = models.CharField(max_length=50, choices=UsersRoleChoices.choices)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return f'{self.fullname}'
