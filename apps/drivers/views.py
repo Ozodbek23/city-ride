43b985c05ead37630bf6a6234bf10684d8c6392b
shohcodes <zokirislom01@gmail.com>
2023-10-23 17:32:15 +0500

    new viewsets added

--- apps/drivers/views.py
+++ apps/drivers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from apps.drivers.models import Drivers, CarModels
+from apps.drivers.serializers import DriversSerializers, CarModelsSerializers
+
+
+class DriversViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet,
+                     mixins.DestroyModelMixin):
+    queryset = Drivers.objects.all()
+    serializer_class = DriversSerializers
+    permission_classes = [IsAuthenticated]
+
+
+class CarModelViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet,
+                      mixins.DestroyModelMixin):
+    queryset = CarModels.objects.all()
+    serializer_class = CarModelsSerializers
+    permission_classes = [IsAuthenticated]
