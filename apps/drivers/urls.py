54f769c8ec87c3691ec6bacf09e8aaa472d7cdc6
shohcodes <zokirislom01@gmail.com>
2023-10-23 17:32:00 +0500

    new urls included

--- apps/drivers/urls.py
+++ apps/drivers/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = [
+from rest_framework.routers import DefaultRouter
 
-]
+from apps.drivers.views import DriversViewSet, CarModelViewSet
+
+router = DefaultRouter()
+
+router.register(prefix='drivers', viewset=DriversViewSet, basename='drivers')
+router.register(prefix='car_models', viewset=CarModelViewSet, basename='car_models')
+
+urlpatterns = router.urls
