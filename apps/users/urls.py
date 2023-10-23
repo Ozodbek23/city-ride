e2e7704e799ba677f14ae8a9869e38b0d27e71c6
shohcodes <zokirislom01@gmail.com>
2023-10-23 17:33:50 +0500

    new urls included

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from rest_framework import routers
 
-]
+from apps.users.views import UsersViewSet
+
+router = routers.DefaultRouter()
+
+router.register(prefix='users', viewset=UsersViewSet, basename='users')
+
+urlpatterns = router.urls
