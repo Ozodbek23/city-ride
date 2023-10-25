0347f678914325a62b09c483bea2c553de7cf84b
shohcodes <zokirislom01@gmail.com>
2023-10-25 17:11:11 +0500

    user create serializer created

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,9 +1,21 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
-from apps.users.models import Users
+from apps.users.models import User
 
 
 class UsersSerializers(serializers.ModelSerializer):
     class Meta:
-        model = Users
-        fields = '__all__'
+        model = User
+        fields = ['id', 'full_name', 'phone_number', 'role']
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['full_name', 'phone_number', 'password']
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        instance = User.objects.create(**validated_data)
+        return instance
