980087d335fa2fbed68fe2631cfdad62c5a6eb36
shohcodes <zokirislom01@gmail.com>
2023-10-25 17:11:34 +0500

    get-me updated

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,15 +4,20 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from apps.users.models import Users
-from apps.users.serializers import UsersSerializers
+from apps.users.models import User
+from apps.users.serializers import UsersSerializers, UserCreateSerializer
 
 
 class UsersViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet,
                    mixins.DestroyModelMixin):
-    queryset = Users.objects.all()
+    queryset = User.objects.all()
     serializer_class = UsersSerializers
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserCreateSerializer
+        return self.serializer_class
 
 
 @api_view(['GET'])
@@ -21,8 +26,8 @@ def get_me(request):
     user = request.user
     user_data = {
         'id': user.id,
-        'first_name': user.first_name,
-        'username': user.username,
+        'full_name': user.full_name,
+        'phone_number': user.phone_number,
     }
 
     return Response(user_data)
