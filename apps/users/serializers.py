0d3d8e4077c5bc25034ba84703f359d1643ec46c
shohcodes <zokirislom01@gmail.com>
2023-11-02 15:48:05 +0500

    serializers updated

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -14,8 +14,3 @@ class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['full_name', 'phone_number', 'role']
-
-    def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
-        instance = User.objects.create(**validated_data)
-        return instance
