4ca3abdfc1b79c38e22e1443023f332cc09a3483
shohcodes <zokirislom01@gmail.com>
2023-11-11 17:42:42 +0500

    views changed

--- apps/drivers/views.py
+++ apps/drivers/views.py
@@ -1,9 +1,12 @@
+import json
+
 from rest_framework import mixins
+from rest_framework.parsers import MultiPartParser, FileUploadParser, JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
 from apps.drivers.models import Drivers, CarModels
-from apps.drivers.serializers import DriversSerializers, CarModelsSerializers
+from apps.drivers.serializers import DriversSerializers, CarModelsSerializers, DriverCreateSerializer
 
 
 class DriversViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet,
@@ -11,6 +14,12 @@ class DriversViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.
     queryset = Drivers.objects.all()
     serializer_class = DriversSerializers
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser]
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return DriverCreateSerializer
+        return self.serializer_class
 
 
 class CarModelViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet,
