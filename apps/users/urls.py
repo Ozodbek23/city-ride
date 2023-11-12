315f78928db7f2d583bedce060773479bda3f7bc
shohcodes <zokirislom01@gmail.com>
2023-11-12 23:45:42 +0500

    new verify sms url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,12 @@
+from django.urls import path
 from rest_framework import routers
 
-from apps.users.views import UsersViewSet
+from apps.users.views import UsersViewSet, VerifyUsersAPIView
 
 router = routers.DefaultRouter()
 
 router.register(prefix='users', viewset=UsersViewSet, basename='users')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('verify_user/', VerifyUsersAPIView.as_view(), name='verify_user')
+] + router.urls
