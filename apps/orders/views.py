68a850668f3d3d874cae4ec51e799e82fe87a5e8
shohcodes <zokirislom01@gmail.com>
2023-11-21 14:18:35 +0500

    views updated: list func added

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,8 +1,11 @@
+from django.db.models import Q
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from apps.orders.filters import Order1Filter
+from apps.orders.filters import OrderFilter
+# from apps.orders.filters import Order1Filter
 from apps.orders.models import DriverOrders, ClientOrder
 from apps.orders.permissions import DriverOrderPermission
 from apps.orders.serializers import DriverOrdersSerializers, ClientOrdersSerializers, DriverOrdersCreateSerializer, \
@@ -14,8 +17,9 @@ class DriverOrdersViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mi
     queryset = DriverOrders.objects.all()
     serializer_class = DriverOrdersSerializers
     permission_classes = [IsAuthenticated, DriverOrderPermission]
-    # filter_backends = [OrderFilter]
-    filter_class = Order1Filter
+    filter_backends = [OrderFilter]
+
+    # filter_class = Order1Filter
 
     def get_serializer_class(self):
         if self.action == 'create':
@@ -35,3 +39,9 @@ class ClientOrdersViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mi
         if self.action == 'create':
             return ClientOrdersCreateSerializers
         return self.serializer_class
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset = queryset.filter(Q(client=request.user))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
