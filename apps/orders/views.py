8716f6d8fee0b1fdd2695b55f3a08a40497939f9
shohcodes <zokirislom01@gmail.com>
2023-11-28 18:11:19 +0500

    ClientOrderAcceptViewSet added

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
 from rest_framework import mixins
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -9,14 +10,15 @@ from apps.orders.filters import OrderFilter
 from apps.orders.models import DriverOrders, ClientOrder
 from apps.orders.permissions import DriverOrderPermission
 from apps.orders.serializers import DriverOrdersSerializers, ClientOrdersSerializers, DriverOrdersCreateSerializer, \
-    ClientOrdersCreateSerializers, DriverOrdersListSerializers
+    ClientOrdersCreateSerializers, DriverOrdersListSerializers, ClientOrdersAcceptSerializer
+from apps.users.choices import UsersRoleChoices
 
 
 class DriverOrdersViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet,
                           mixins.DestroyModelMixin):
     queryset = DriverOrders.objects.all()
     serializer_class = DriverOrdersSerializers
-    permission_classes = [IsAuthenticated, DriverOrderPermission]
+    # permission_classes = [IsAuthenticated, DriverOrderPermission]
     filter_backends = [OrderFilter]
 
     # filter_class = Order1Filter
@@ -42,6 +44,15 @@ class ClientOrdersViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mi
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-        queryset = queryset.filter(Q(client=request.user))
+        if request.user.role == UsersRoleChoices.ADMIN:
+            queryset = self.get_queryset()
+        else:
+            queryset = queryset.filter(Q(client=request.user))
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class ClientOrderAcceptViewSet(UpdateAPIView):
+    queryset = ClientOrder.objects.all()
+    serializer_class = ClientOrdersAcceptSerializer
+    permission_classes = [IsAuthenticated]
